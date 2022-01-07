ab432a772dde26b664b5d877b26786fe8a0c559c
冬茶 <27325188+wintertee@users.noreply.github.com>
2022-01-07 21:33:58 +0100

    delete useless code, add comments

--- src/comicbook.h
+++ src/comicbook.h
@@ -1,9 +1,6 @@
 #ifndef COMICBOOK_H
 #define COMICBOOK_H
 
-#include <cstddef>
-#include <map>
-#include <string>
 #include <vector>
 
 typedef unsigned char byte_t;
@@ -11,12 +8,21 @@ typedef unsigned char byte_t;
 class ComicBook {
   public:
     ComicBook();
+
+    /// append data to content
     void append_content(std::vector<byte_t> &data);
+
+    /// return content.data()
     const std::vector<byte_t> *get_content(void) const;
+
+    /// return content.size()
     size_t size() const;
+
+    /// return content.empty()
     bool empty() const;
 
   private:
+    /// elements are images
     std::vector<std::vector<byte_t>> content;
 };
 
